10d8c1a1e806906044d7760c92e1d550289056b0
changjianhong <cjh130@126.com>
2016-03-22 15:18:34 +0800

    add BannerCarouse by swift

---
+++ BannerCarouseView/BannerCarouseView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BannerCarouseView
+//
+//  Created by changjianhong on 16/3/22.
+//  Copyright © 2016年 changjianhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
